886285fb2658f385388a96b2858d4ce1447109e7
Fernando Barbona <Fernando.Barbona@alumnos.uai.edu.ar>
2023-05-08 15:08:57 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoTiendaVinos/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoTiendaVinos.Models;
+using System.Diagnostics;
+
+namespace ProyectoTiendaVinos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+        public IActionResult Productos()    
+        {
+            return View();
+        }
+        public IActionResult Carrito()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
